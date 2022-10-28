ee7a9cc0e4479f3a4b8533f0f992944261570e6e
frejaz <fhpe19@student.aau.dk>
2022-10-28 09:36:21 +0200

    researched parser

--- src/infrastructure/verifyta_environment.ts
+++ src/infrastructure/verifyta_environment.ts
@@ -1,4 +1,6 @@
 import { VerifytaResult } from './verifyta_result';
+import { exec } from 'child_process';
+import { Console } from 'console';
 
 export class VerifytaEnvironment {
   private readonly _result?: VerifytaResult;
@@ -9,10 +11,37 @@ export class VerifytaEnvironment {
   }
 
   execute(xmlFile: string): string {
-    throw new Error('not Implemented');
+    //throw new Error('not Implemented');
+    exec(
+      '-Command C:\\Users\\freja\\OneDrive\\Dokumenter\\AAU\\6. semester\\MTPCS\\uppaal64-4.1.26\\bin-Windows\\verifyta.exe -u C:\\Users\\freja\\Desktop\\Verifiers\\src\\test_files\\lightswitch_syntaxError.xml',
+      { shell: 'powershell.exe' },
+      (error, stdout, stderr) => {
+        stdout.on('data', (data: string) => {Console.log(`Received chunk ${data}`);
+      });
+      }
+    );
+    return 'hello!';
+    // C:\\Users\\freja\\OneDrive\\Dokumenter\\AAU\\6. semester\\MTPCS\\uppaal64-4.1.26\\bin-Windows\\verifyta.exe", "-u", "C:\\Users\\freja\\Desktop\\Verifiers\\src\\test_files\\lightswitch_syntaxError.xml"};
+
+    // let spawn = require('child_process').spawn;
+    // child = spawn('powershell.exe', ['-Command', 'C:\\Users\\freja\\OneDrive\\Dokumenter\\AAU\\6. semester\\MTPCS\\uppaal64-4.1.26\\bin-Windows\\verifyta.exe', '-u', 'C:\\Users\\freja\\Desktop\\Verifiers\\src\\test_files\\lightswitch_syntaxError.xml']);
+
+
+    // child.stdout.on('data', function (data) {
+    //   console.log('Powershell Data: ' + data);
+    // });
+    // child.stderr.on('data', function (data) {
+    //   console.log('Powershell Errors: ' + data);
+    // });
+    // child.on('exit', function () {
+    //   console.log('Powershell Script finished');
+    // });
+    // child.stdin.end(); //end input
   }
 
   get result(): VerifytaResult | undefined {
     return this._result;
   }
 }
+
+//{"powershell.exe", "-Command", "C:\\Users\\freja\\OneDrive\\Dokumenter\\AAU\\6. semester\\MTPCS\\uppaal64-4.1.26\\bin-Windows\\verifyta.exe", "-u", "C:\\Users\\freja\\Desktop\\Verifiers\\src\\test_files\\lightswitch_syntaxError.xml"};
